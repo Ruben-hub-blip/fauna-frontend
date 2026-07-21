6b2c47e3a9384f85c5520e04791c7fd5b151e310
Ruben-hub-blip <rubenarangomiranda@gmail.com>
2026-07-21 17:40:43 -0500

    Import environment in enciclopedia.service.ts
    Added import for environment configuration.

--- src/app/features/enciclopedia/services/enciclopedia.service.ts
+++ src/app/features/enciclopedia/services/enciclopedia.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
 
 // Interfaz del Frontend (Mantenemos tus nombres para no dañar tu HTML)
 export interface Especie {
